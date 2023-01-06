f4d58dde07880ee8b519a3a7e24f5d521185e1fe
Wojtek <118207811+W-JK@users.noreply.github.com>
2023-01-06 18:42:51 +0000

    update - task structure

--- index.js
+++ index.js
@@ -86,3 +86,49 @@ var finances = [
 ['Jan-2017', 138230],
 ['Feb-2017', 671099]
 ];
+
+
+//1) the total number of months included in the dataset:
+
+
+
+
+//2) total amount of profit/loses for entire period
+
+
+
+
+// 3) the average of the changes in Profit/Losses over entire period 
+/*   * You will need to track what the total change in profits are from month to month and then find the average.
+  * (`Total/Number of months` */ 
+
+
+
+
+// 4)  The greatest increase in profits (date and amount) over the entire period.
+
+
+
+//  The greatest decrease in losses (date and amount) over the entire period.
+
+
+
+/* When you open your code in the browser your resulting analysis should look similar to the following:
+
+  ```text
+  Financial Analysis
+  ----------------------------
+  Total Months: 25
+  Total: $2561231
+  Average  Change: $-2315.12
+  Greatest Increase in Profits: Feb-2012 ($1926159)
+  Greatest Decrease in Profits: Sep-2013 ($-2196167)
+  ```   */ 
+
+
+// ! Your final code should print the analysis to the console. !
+
+
+
+
+
